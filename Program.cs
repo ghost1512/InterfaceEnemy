678d536e1e93ee66db8e71baf292d6af2ee57e5d
BaoHG <baohuynh291@gmail.com>
2024-04-25 19:42:09 +0700

    add
    add

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Wood wood = new Wood();
+        wood.Efect();
+    }
+}
+
+public interface IDamger
+{
+    void Efect();
+}
+
+public class Wood : IDamger
+{
+    public void Efect()
+    {
+        Console.WriteLine("Tao lo tren manh go");
+    }
+}
+public class Iron : IDamger
+{
+    public void Efect()
+    {
+        Console.WriteLine("Tao tia lua");
+    }
+}
+public class Grass : IDamger
+{
+    public void Efect()
+    {
+        Console.WriteLine("Tao bun dat ban len");
+    }
+}
+public class Bullet
+{
+    Wood bulletCollider = new Wood();
+
+}
